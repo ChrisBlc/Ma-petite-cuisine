89601447984e7ed6f71c7ed8e7841027911de43b
Bugz3 <u.gaigneux@outlook.fr>
2022-05-24 12:03:59 +0200

    Modif fichier en PHP

---
+++ pageRecette.php
@@ -0,0 +1,84 @@
+<?php 
+require_once("banniere.php");
+?>
+
+    <div class="corpsPageRecette">
+        <div class="bandeau">
+            <h2 class="section">Bo Bun &nbsp</h2>
+            <h3 class="username">Nom d'utilisateur</h3>
+        </div>
+        <h3 class="h3Bandeau">35 minutes  |  Facile  |  Sans porc |  Bon marché |  Toutes saisons </h3>
+        <div class="divPhoto">
+            <img class="photoPrincipale" src="img/PhotoRecettes/bobun.jpeg" alt="Photo de Bo bun">
+        </div>
+        <div class="bandeau">
+            <h2>Ingrédients</h2>
+        </div>
+        <div class="buttons">
+            <div class="nbrPersonnes">
+                <p><button class="plusEtMoins"> - </button> 4 personnes <button class="plusEtMoins"> + </button></p>
+            </div>
+            <div class="buttonCourses">
+                <button class="ajouterListe">Ajouter à ma liste</button>
+            </div>
+        </div>
+        <div class="listIngredients">
+            <div class="cardIngredients">
+                <img src="img/PhotosIngredients/Boeuf.jpeg" alt="700g de boeuf tendre"/>
+                <p>700g de boeuf tendre</p>
+            </div>
+            <div class="cardIngredients">
+                <img src="img/PhotosIngredients/Curry.jpg" alt="1 CS de curry doux"/>
+                <p>1 CS de curry doux</p>
+            </div>
+            <div class="cardIngredients">
+                <img src="img/PhotosIngredients/Ail.jpg" alt="1 gousse d'ail"/>
+                <p>1 gousse d'ail</p>
+            </div>
+            <div class="cardIngredients">
+                <img src="img/PhotosIngredients/Sauce_huitre.jpeg" alt="3 CS de sauce d'huitre"/>
+                <p>3 CS de sauce d'huitre</p>
+            </div>
+            <div class="cardIngredients">
+                <img src="img/PhotosIngredients/Poivre.jpg" alt="Poivre"/>
+                <p>Poivre</p>
+            </div>
+            <div class="cardIngredients">
+                <img src="img/PhotosIngredients/Sel.jpg" alt="Sel"/>
+                <p>Sel</p>
+            </div>
+            <div class="cardIngredients">
+                <img src="img/PhotosIngredients/Coriandre.jpg" alt="Coriandre"/>
+                <p>Coriandre</p>
+            </div>
+            <div class="cardIngredients">
+                <img src="img/PhotosIngredients/Batavia.jpeg" alt="Batavia"/>
+                <p>Batavia</p>
+            </div>
+            <div class="cardIngredients">
+                <img src="img/PhotosIngredients/Menthe.jpeg" alt="Menthe"/>
+                <p>Menthe</p>
+            </div>
+            <div class="cardIngredients">
+                <img src="img/PhotosIngredients/Soja.jpeg" alt="300g de soja frais"/>
+                <p>300g de soja frais</p>
+            </div>
+            <div class="cardIngredients">
+                <img src="img/PhotosIngredients/Arachide.jpeg" alt="Arachide"/>
+                <p>Arachide</p>
+            </div>
+
+
+
+
+        </div>
+
+
+
+    </div>
+
+
+
+    
+</body>
+</html>
