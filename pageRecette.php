8c1189a9268e695c990333ad9a1cc61c4eaa9acc
Bugz3 <u.gaigneux@outlook.fr>
2022-05-24 16:57:20 +0200

    Reste a faire commentaires page recette

--- pageRecette.php
+++ pageRecette.php
@@ -4,8 +4,20 @@ require_once("banniere.php");
 
     <div class="corpsPageRecette">
         <div class="bandeau">
-            <h2 class="section">Bo Bun &nbsp</h2>
-            <h3 class="username">Nom d'utilisateur</h3>
+            <div class="bandeauGauche">
+                <h2 class="section">Bo Bun &nbsp</h2>
+                <h3 class="username">Par nom d'utilisateur</h3>
+            </div>
+            <div class="bandeauDroite">
+                <div class="etoilesRecette">
+                    <img src='Img/etoile.png' alt='etoile'>
+                    <img src='Img/etoile.png' alt='etoile'>
+                    <img src='Img/etoile.png' alt='etoile'>
+                    <img src='Img/etoile.png' alt='etoile'>
+                    <img src='Img/etoile.png' alt='etoile'> 
+                </div>
+                <h3 class="avis">82 avis</h3>
+            </div>
         </div>
         <h3 class="h3Bandeau">35 minutes  |  Facile  |  Sans porc |  Bon marché |  Toutes saisons </h3>
         <div class="divPhoto">
@@ -67,18 +79,45 @@ require_once("banniere.php");
                 <img src="img/PhotosIngredients/Arachide.jpeg" alt="Arachide"/>
                 <p>Arachide</p>
             </div>
-
-
-
-
         </div>
+        <div class='bandeau'>
+            <h2>Etape 1 :</h2>
+        </div>
+        <p class="pEtapes">Cisailler la viande en tranches très fines, et faire mariner au frigo avec le curry, ail,
+             glutamate et les sauces, au moins 30 mn.</p>
+        <div class='bandeau'>
+            <h2>Etape 2 :</h2>
+        </div>
+        <p class="pEtapes">Pendant ce temps préparer la sauce, si vous cuisinez souvent asiatique, doubler les proportions et conservez-là dans un bocal. Vous pouvez ajouter une pointe de piment (sauce chili) pour la parfumer. Faire bouillir tous les ingrédients et réserver.</p>
+        <div class='bandeau'>
+            <h2>Etape 3 :</h2>
+        </div>
+        <p class="pEtapes">Faire revenir la viande dans un peu d'huile, à feu très fort et très brièvement : la viande doit être saisie simplement, pour la garder tendre.</p>
+        <div class='bandeau'>
+            <h2>Etape 4 :</h2>
+        </div>
+        <p class="pEtapes">Servir dans des grand bols de préférence : commencer par la batavia, le soja, puis 3 ou 4 poignées de vermicelles par personnes, 2 nems coupés petits morceaux par personne, la viande. Décorer avec la coriandre hachée, la menthe et quelques cacahuètes concassées (facultatif). Arroser de sauce et c'est prêt !</p>
+        <div class="buttonExporter">
+            <button class="exporter">Exporter en PDF</button></div>
+        </div>
+        <div class="blockCommentaires">
+            <div class="fondCommentaires">
+                <div class="bandeau rouge">
+                    <h2>Commentaires</h2>
+                </div>
+                <div class="commentairesRecette">
 
 
+                </div>
 
-    </div>
-
-
-
+            </div>
+        </div>
     
+    
+    
+    
+    </div>
 </body>
 </html>
+
+<?php require_once("piedPage.php"); ?>
