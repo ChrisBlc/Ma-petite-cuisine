8a6219c4ce9aa589898db85d453340f8e5a4fe84
ChrisBlc <christopherblanc64@gmail.com>
2022-05-24 08:57:06 +0200

    ajout menu burger et menu burger VM

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 $(function(){
-    $('.menuBurger').hide();
-    $(window).scroll( function() {
-      var value = $(this).scrollTop();
-      if ( value > 400 ){
-        $("header").addClass("smallHeader");
-        $('body').css('margin-top', '400px');
-      }
+
+  
+    $('.burger').click(function () { 
+      $('.menuBurger').addClass('menuBurgerVisible');
+    });
+
+    $('.croix').click(function() {
+        $(".menuBurger").removeClass('menuBurgerVisible');
+    })
+
+
+    
+    $(document).ready(function(){
+      if($(window).width() > 670) {
+        $(window).scroll( function() {
+            var position = $(this).scrollTop();
+            if ( position > 400 ){
+              $("header").addClass("smallHeader");
+              $('body').css('margin-top', '400px');
+            }
+            else{
+              $("header").removeClass("smallHeader");
+              $('body').css('margin-top', 0);
+            }
+        })
+      } 
       else{
-        $("header").removeClass("smallHeader");
-        $('body').css('margin-top', 0);
+        $("header").addClass("smallHeader");
       }
-          
-  });
+    });
 });
+     
+    
+    
+    
