15c0022b45aa7653c4c8943a53a2b91ba5b8f50a
philippe-30 <hassan.philippe@yahoo.com>
2022-06-03 10:05:27 +0200

    travail01-06

--- propositionRecette.php
+++ propositionRecette.php
@@ -1,7 +1,7 @@
 <?php 
 require_once("banniere.php");?>
 
-  <form action="#">
+  <form action="#" method="post" enctype="multipart/from-data">
     <div class="proTitre">
         <input type="text" placeholder="comment s'appelle votre recette? (Max 60 caractères)">
     </div>
@@ -9,13 +9,13 @@ require_once("banniere.php");?>
         <div class="selection">
             <h2>Type de plat :</h2>
             <div>
-                <input type="checkbox" name="typePlat" id="aperitif" value="1">
+                <input type="checkbox" name="categorie" id="aperitif" value="1">
                     <label for="aperitif">Apéritif</label><br>
-                <input type="checkbox" name="typePlat" id="entree" value="2">
+                <input type="checkbox" name="categorie" id="entree" value="2">
                     <label for="entree">Entrée</label><br>
-                <input type="checkbox" name="typePlat" id="plat" value="3">
+                <input type="checkbox" name="categorie" id="plat" value="3">
                     <label for="plat">Plat</label><br>
-                <input type="checkbox" name="typePlat" id="dessert" value="4">
+                <input type="checkbox" name="categorie" id="dessert" value="4">
                     <label for="dessert">Dessert</label>
             </div>
         </div>
@@ -49,26 +49,27 @@ require_once("banniere.php");?>
     <div class="selections">
         <select class="menuDeroulant" name="cout" id="bon">
             <option value="">--choisir un coût--</option>
-            <option value="bonMarche">Bon Marché</option>
-            <option value="coutmoyen">Coût Moyen</option>
-            <option value="assezCher">Assez Cher</option>
+            <option value="1">Bon Marché</option>
+            <option value="2">Coût Moyen</option>
+            <option value="3">Assez Cher</option>
         </select>
         <input class="selectionquantite" type="number" name="parts" id="personnePart" placeholder="Nombre de parts" min="1">
         
         <select class="menuDeroulant" name="difficulte" id="dificile">
             <option value="">--choisir un Facilité--</option>
-            <option value="facile">Facile</option>
-            <option value="moyen">Moyen</option>
-            <option value="dificile">Dificile</option>
+            <option value="1">Facile</option>
+            <option value="2">Moyen</option>
+            <option value="3">Dificile</option>
         </select>
     </div>
     <div class="selections">
-        <div>
+        <div class="tempsbuton">
             <label class="selectionTemps" for="tempsPreparation">--Temps de Préparation:</label>
-            <input type="time" id="tempsPreparation" name="temps" min="00:05" max="04:00" required>
-
+            <input type="time" id="tempsPreparation" name="tempsPrep" min="00:05" max="04:00" required>
+        </div>
+        <div class="tempsbuton">
             <label class="selectionTemps" for="tempscuisson">--Temps de Cuisson:</label>
-            <input type="time" id="tempscuisson" name="temps" min="00:05" max="05:00" required>
+            <input type="time" id="tempscuisson" name="tempsCui" min="00:05" max="05:00" required>
         </div>
     </div>
     
@@ -76,13 +77,13 @@ require_once("banniere.php");?>
             <div class="ajoutingredient">
                 <select class="menuDeroulant" name="ingredient" id="ingred">
                     <option value="">--choisir un ingredient--</option>
-                    <option value="tomate">tomate</option>
-                    <option value="huile">huile d'olive</option>
-                    <option value="mozarella">mozarella</option>
-                    <option value="basilic">Basilic</option>
+                    <option value="2">tomate</option>
+                    <option value="334">huile d'olive</option>
+                    <option value="34">mozarella</option>
+                    <option value="4343">Basilic</option>
                 </select>
-                <input class="selectionquantite" type="number" name="quantite" id="unite" placeholder="Quantité d'ingredient" min="1" step="0.1">
-                <button class="selectionAjouter" type="submit" name="submit">Ajouter</button>
+                <input class="selectionquantite" type="number" name="quantite" id="unite" placeholder="Quantité d'ingredient" min="0" step="0.5">
+                <button class="selectionAjouter"  name="ajoutIngredient">Ajouter</button>
             </div>
                 <div class="ingredientAjout">
                     <div class="listeIngredient">
@@ -94,19 +95,22 @@ require_once("banniere.php");?>
                         </dt>
                     </div>
                     <div class="ajoutNouvelIngredient">
-                        <button class="selectionAjouter" type="submit" name="submit">Nouvel ingrédient</button>
-                        <button class="selectionAjouter" type="submit" name="submit">Ajouter l'ingrédient</button>
+                        <input class="selectionAjouter" type="text" name="nouvelIngredient"  placeholder="Nouvel ingrédient"/>
+                        <button class="selectionAjouter" type="submit"  name="ajoutNouvelIngredient">Ajouter l'ingrédient</button>
                     </div>
                 </div>
             </div>
-     <div class="selectionAjouterEtape">
-        <textarea name="textarea" placeholder="Etape1: Rentrez vous instructions" rows="5" cols="180"></textarea>
-        <textarea name="textarea" placeholder="Etape2: Rentrez vous instructions" rows="5" cols="180"></textarea>
-        <textarea name="textarea" placeholder="Ajouter une étape" rows="5" cols="180"></textarea>
-        <textarea name="textarea" placeholder="Ajouter une photo" rows="5" cols="180"></textarea>
-     </div>
+    <div class="selectionAjouterEtape">
+        <textarea name="etape1" placeholder="Etape1: Rentrez vous instructions" rows="5" cols="180"></textarea>
+        <textarea name="etape2" placeholder="Etape2: Rentrez vous instructions" rows="5" cols="180"></textarea>
+        <div class="etapes"><a class="ajouter ajoutEtape"  href="#">Ajouter une etape</a></div>
+    </div>
+    <div class="selectionAjouterEtape">
+        <input id='ajoutPhoto' class="etapes" type="file" name="photoRecette"/>
+    </div>
+        
      <div class="validerRecette">
-        <button class="selectionAjouter" type="submit" name="submit">Valider la recette</button>
+        <button class="selectionAjouter" type="submit" name="valider">Valider la recette</button>
      </div>
 </form>
 
